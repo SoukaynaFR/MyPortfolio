dfc0d6aa5234e444dbb245423277ef844da5ff9a
Soukayna El Ferchouni <soukaynafr2019@gmail.com>
2023-10-29 16:08:49 +0100

    adding Loading on scrolling functionnality

--- js/script.js
+++ js/script.js
@@ -118,4 +118,20 @@ function sendMail() {
   }
 }
 
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }
+    else{
+      entry.target.classList.remove('show');
+    }
+  })
+})
 
+
+
+
+const hiddenElements = document.querySelectorAll('.AOS');
+hiddenElements.forEach((el) => observer.observe(el));
